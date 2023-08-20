b40e34ed1b1729062496bfd556ccaf34eb503f5f
Jonathan <jjames422@me.com>
2023-08-20 07:03:08 -0400

    Layout

--- app/ui/Packages.jsx
+++ app/ui/Packages.jsx
@@ -1,4 +1,5 @@
 import TextBoxBreak from "@/componentsTextBoxBreak";
+import TextBoxFlip from "@/componentsTextBoxFlip";
 
 export default function Packages() {
     return (
@@ -7,6 +8,7 @@ export default function Packages() {
         <div className="bg-motteal-700 text-white">
             <h1>Who doesn't like big Packages?</h1>
         </div>
+        <TextBoxFlip className="fill-motteal-700 bg-motdark"/>
         </div>
     );
 }
